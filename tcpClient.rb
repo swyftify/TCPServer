f7299ef5eb88c1d1921629cdbc7fcf96146c7033
Pavel <tarasovp@tcd.ie>
2015-10-16 11:13:18 +0100

    Updated server and client with limited number of threads

--- tcpClient.rb
+++ tcpClient.rb
@@ -4,8 +4,14 @@ port = 2000
 host = 'localhost'
 
 socket = TCPSocket.open(host,port)
-socket.puts "Connected"
-while line = socket.gets
-	puts line.chop
+puts socket.gets
+input = gets.chomp
+while input != "end"
+	socket.puts(input)
+	print "SERVER SAID\n"
+	puts socket.gets
+	input = gets.chomp
+	socket.puts(input)
 end
+puts "Disconnected"
 socket.close
--- tcpServer.rb
+++ tcpServer.rb
@@ -1,39 +1,97 @@
 require 'socket'
 require 'thread'
 
+unless ARGV.length == 1
+  print "The correct number of arguments is 1!\n"
+  exit
+end
 
-server = TCPServer.new 2000
+port = ARGV[0]
+puts "Servers started on port #{port}"
+
+server = TCPServer.new port
 activeQ = Queue.new
+lock = Mutex.new
 
-loop do
-	Thread.start(server.accept) do |client| 
-    print client.gets
-    activeQ.push(client)
-    loop do
-      workers = (0...2).map do  
-        Thread.new do
-          begin
-            if activeQ.empty?
-              print "Sleeping #{Thread.current}\n"
-              sleep(1)
-            else
-              while !activeQ.empty?
-                var = activeQ.pop()
-                var.puts "Hello !"
-                var.puts "Time is #{Time.now}"
-                sleep(4)
-                print "Disconnecting Client\n"
-                var.puts "Disconnected"
-                var.close
-              end
-            end
+threadNum = 0
+killSwitch = false
+
+Thread.new do
+  loop do
+    Thread.start(server.accept) do |client|
+      activeQ.push(client)
+    end
+  end  
+end
+
+Thread.new do
+  loop do
+    if !activeQ.empty? and threadNum < 3
+      Thread.new do
+        threadNum+=1
+        client = activeQ.pop()
+        client.puts("#{Time.now}")
+        puts "#{threadNum}"
+        puts "Client active"
+        clientRunning = true
+        while clientRunning
+          input = client.gets
+          puts input
+          if input.start_with?("HELO")
+            puts input
+            helo, text = input.split(" ",2)
+            puts "#{text}"
+            client.puts "HELO #{text}"
+          elsif input == "KILl_SERVICE\n"
+            client.puts "KILL"
+            killSwitch = true
+            clientRunning = false       
           end
         end
+        if killSwitch == true
+          exit
+        end
+        threadNum -=1
+        client.close
       end
-    workers.map(&:join)
+    else
+      sleep(1)  
+    end  
+  end 
+end
+
+while true
+
+end
+
+
+
+
+
+=begin
+  
+workers = (0...2).map do  
+  Thread.start(server.accept) do |client|
+    print client.gets
+    input = client.gets
+    client.puts "Hello !"
+    client.puts "Time is #{Time.now}"
+    clientRunning = true;
+    while clientRunning
+      if input.start_with?("HELO")
+        client.puts "Hello"
+      elsif input == "KIL"
+        client.puts "KILL"
+        clientRunning = false
+      else
+        puts input    
+      end
     end
   end
 end
+workers.map(&:join)
+
+=end
 
 
 
